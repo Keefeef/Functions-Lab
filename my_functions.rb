551af30c91d48c20567755f50091074b580c2e78
Ethan Radcliffe <ethanr@mac.com>
2019-01-16 16:19:50 +0000

    add item false tested

--- my_functions.rb
+++ my_functions.rb
@@ -6,3 +6,22 @@ def add_array_lengths(fruit_1, fruit_2)
   num = fruit_1.length.to_i + fruit_2.length.to_i
   return num
 end
+
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+  return total
+end
+
+def find_item(array,house)
+  for house in array
+    if
+      house == array
+      return true 
+    end
+    return false
+  end
+end
